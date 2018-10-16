19ff8e71552e69b6cd5d9ab5e18a69a72b676d7b
赵权洲 <1747587818@qq.com>
2018-10-16 19:07:52 +0800

    UI Signed-off-by: 赵权洲 <1747587818@qq.com>

---
+++ UIDemoOne/Assets/Scripts/OpenTips.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTips : MonoBehaviour
+{
+    public GameObject openTips;
+    public GameObject targetBuotton;
+
+    private void Start()
+    {
+    }
+
+    public void OnClickButton()
+    {
+        openTips.SetActive(true);
+        TipsManeger.Instance.targetBuotton = targetBuotton;
+    }
+}
---
+++ UIDemoOne/Assets/Scripts/TipsManeger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipsManeger : MonoBehaviour
+{
+    #region 单例模式
+
+    private static TipsManeger instance;
+
+    public static TipsManeger Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(TipsManeger)) as TipsManeger;
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    instance = obj.AddComponent<TipsManeger>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    #endregion 单例模式
+
+    public delegate void DestroyGameobject();
+
+    public GameObject targetBuotton;
+
+    public UILabel targetLabel;
+    private UILabel label;
+
+    private void Start()
+    {
+        label = gameObject.GetComponentInChildren<UILabel>();
+        targetLabel = targetLabel.GetComponentInChildren<UILabel>();
+    }
+
+    private void Update()
+    {
+        label.text = targetLabel.text;
+    }
+
+    public void OnClickSure()
+    {
+        targetBuotton.SetActive(false);
+    }
+
+    public void OnClickCancel()
+    {
+        gameObject.SetActive(false);
+    }
+}
