5f31baf83369c8e9af71a8de82d1c045c1a8a861
赵权洲 <1747587818@qq.com>
2018-10-16 17:40:46 +0800

    作业 Signed-off-by: 赵权洲 <1747587818@qq.com>

---
+++ 委托与事件作业/委托与事件作业/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 委托与事件作业
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RunPlayer player = new RunPlayer();
+            Judge judge = new Judge();
+            //订阅事件
+            judge.RunEvent += player.Run;
+            judge.ShootStart();
+        }
+    }
+
+    //被观察的对象 事件发布者
+    internal class Judge
+    {
+        public delegate void RunHandler();
+
+        public event RunHandler RunEvent;//事件对象
+
+        public void ShootStart()
+        {
+            RunEvent();
+        }
+    }
+
+    //观察的对象 事件订阅者
+    internal class RunPlayer
+    {
+        public void Run()
+        {
+            Console.WriteLine("开始奔跑了");
+        }
+    }
+}
