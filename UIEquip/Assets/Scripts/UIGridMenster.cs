c6f10b0af62e1032aca4ae5068b73fe50b99e5f5
赵权洲 <1747587818@qqcom>
2018-10-17 23:47:00 +0800

    作业 Signed-off-by: 赵权洲 <1747587818@qqcom>

---
+++ UIEquip/Assets/Scripts/UIGridMenster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGridMenster : MonoBehaviour
+{
+
+    public GameObject grid;
+    GameObject[] cell;
+    GameObject eauip;
+    GameObject targetCell;
+    void Start()
+    {
+        cell = GameObject.FindGameObjectsWithTag("Cell");
+        eauip = Resources.Load<GameObject>("Equip");
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnClicKCell();
+        }
+    }
+    public void OnClicKCell()
+    {
+        for (int i = 0; i < cell.Length; i++)
+        {
+
+            if (!cell[i].transform.CompareTag("Equip"))
+            {
+                eauip.transform.SetParent(cell[i].transform);
+
+                Instantiate(eauip,cell[i].transform);
+            }
+        }
+    }
+}
